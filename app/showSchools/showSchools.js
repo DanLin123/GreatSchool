a7853e180cdbcb8d04a98ad02fe0578ccfa63f37
ld <lindan-xmu@126.com>
2015-09-13 21:46:48 +0800

    add paging for school list

--- app/showSchools/showSchools.js
+++ app/showSchools/showSchools.js
@@ -7,41 +7,54 @@ angular.module('myApp.showSchools', [
   'resources.school','ngAnimate', 'ui.bootstrap'
 ])
 .controller('showSchoolsController', function ($scope, schoolService, schoolServiceCached) {
+	$scope.currentPage = 1;
+    $scope.pageSize = 10;
+    $scope.maxSize= 10;
 
+    $scope.numberOfPages=function(){
+        return Math.ceil($scope.data.length/$scope.pageSize);                
+    }
 
- $scope.getLogo = function(logo){
- 	if(logo== "")
- 	{
- 		return "asset/no-school-photo.png"
- 	}
- 	else{
- 		return logo
- 	}
- }
-
- $scope.getScore = function(reviews){
- 	var schoolScore = 0;
- 	if(reviews!=null && reviews!=undefined)
- 	{
- 		var reviewLength = reviews.length;
-	 	for(var i=0; i< reviewLength; i++)
+	 $scope.getLogo = function(logo){
+	 	if(logo== "")
 	 	{
-	 		schoolScore += reviews[i].generalScore
+	 		return "asset/no-school-photo.png"
 	 	}
-		schoolScore = schoolScore/reviewLength;
- 	}
- 	return schoolScore;
- }
+	 	else{
+	 		return logo
+	 	}
+	 }
 
- $scope.getReviewsCount = function(reviews){
- 	if(reviews==null && reviews==undefined)
- 	{
- 		return 0;
- 	}
- 	return reviews.length;
- }
+	 $scope.getScore = function(reviews){
+	 	var schoolScore = 0;
+	 	if(reviews!=null && reviews!=undefined)
+	 	{
+	 		var reviewLength = reviews.length;
+		 	for(var i=0; i< reviewLength; i++)
+		 	{
+		 		schoolScore += reviews[i].generalScore
+		 	}
+			schoolScore = schoolScore/reviewLength;
+	 	}
+	 	return schoolScore;
+	 }
 
-  schoolServiceCached.all(function(schools){
-  	$scope.schools = schools;
-  });  
+	 $scope.getReviewsCount = function(reviews){
+	 	if(reviews==null && reviews==undefined)
+	 	{
+	 		return 0;
+	 	}
+	 	return reviews.length;
+	 }
+
+	  schoolServiceCached.all(function(schools){
+	  	$scope.schools = schools;
+	  });  
 })
+.filter('startFrom', function() {
+    return function(input, start) {
+    	debugger;
+        start = +start; //parse to int
+        return input.slice(start);
+    }
+});
