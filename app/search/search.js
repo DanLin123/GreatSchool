65ad6330c2efa3d8f38370ae7b60be8531223ebb
ld <lindan-xmu@126.com>
2015-09-27 00:12:16 +0800

    seperate search as individual module

---
+++ app/search/search.js
@@ -0,0 +1,71 @@
+'use strict';
+
+angular.module('myApp.search', [
+  'resources.school','ngAnimate', 'ui.bootstrap'
+])
+.controller('searchController', function ($scope, schoolServiceCached) {
+ 	 $scope.currentCity = "福州"  //to do : auto detect the city by the ip
+ 	 $scope.chinaAreas = {
+		    "福建省": {
+		    	"福州市": [
+			        "鼓楼区",
+			        "台江区",
+			        "仓山区",
+			        "马尾区",
+			        "晋安区",
+			        "福清市",
+			        "长乐市",
+			        "闽侯县",
+			        "连江县",
+			        "罗源县",
+			        "闽清县",
+			        "永泰县",
+			        "平潭县"]
+		    }
+ 	 }
+
+	  schoolServiceCached.all(function(schools){
+	  	$scope.schools = schools;
+	  });
+	  $scope.schoolAreas = $scope.chinaAreas.福建省.福州市
+	  $scope.schoolCategary = ["高中", "初中", "幼儿园", "日托"]
+	  $scope.schoolProperty = ["公立","名办", "私立"]
+	  $scope.schoolLevel = ["示范", "重点", "普通"]
+
+	  $scope.selectedValue = {
+	    areas: ["台江区"], 
+	    categary: [], 
+	    property:[],
+	    level:[]
+
+	  }
+
+	  $scope.showSearchTags = function(selected){
+	  	var searchText = "更多查找条件: ";
+	  	var areas = selected.areas;
+
+	  	for (var i = areas.length - 1; i >= 0; i--) {
+	  		searchText += areas[i];
+	  		searchText += " "; 
+	  	};
+	  	return searchText;
+	  }
+
+	  $scope.searchTagsText = $scope.showSearchTags($scope.selectedValue);
+	  
+})
+.filter("getAdvancedSearch", function(){
+   return function(input){
+	var output = "其他查找条件: ";
+   	for (var key in input) {
+	  if (input.hasOwnProperty(key)) {
+	     var options = input[key];
+	     for (var i = options.length - 1; i >= 0; i--) {
+	   	 	output += options[i] + " ";
+	   	 };
+	  }
+	}
+   
+    return output; 
+   }
+});
