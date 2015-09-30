79c5601bcbeee5e79dd37a2a3dac742c711213d4
ld <lindan-xmu@126.com>
2015-09-30 11:22:35 +0800

    remove node_models from git

---
+++ tests/demo.tests.js
@@ -0,0 +1,14 @@
+'use strict';
+
+describe("A suite", function() {
+  it("contains spec with an expectation", function() {
+    expect(true).toBe(true);
+  });
+});
+
+
+describe("A suite2", function() {
+  it("contains spec with an expectation", function() {
+    expect(true).toBe(true);
+  });
+});
