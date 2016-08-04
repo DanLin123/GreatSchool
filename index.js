4e348ec53dc28d1756ae28b6b5e1fc7962af0cc6
lindanqinghua <lindanqinghua@gmail.com>
2016-08-03 18:12:23 -0700

    hard code mongodb URI

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ var router = express.Router();
 
 // Create a database variable outside of the database connection callback to reuse the connection pool in your app.
 var db;
-
+var url = process.env.MONGODB_URI || 'mongodb://heroku_4q70n482:tfo6for71jhjfqjit9bhhnlrob@ds139655.mlab.com:39655/heroku_4q70n482';
 // Connect to the database before starting the application server.
-mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
+mongodb.MongoClient.connect(url, function (err, database) {
   if (err) {
     console.log(err);
     process.exit(1);
@@ -28,8 +28,6 @@ mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
     console.log("App now running on port", port);
   });
 });
-
-
 
 // middleware to use for all requests
 router.use(function(req, res, next) {
