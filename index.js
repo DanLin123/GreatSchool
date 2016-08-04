5e4abd92cbf4304e1024c70b3af341444804d902
lindanqinghua <lindanqinghua@gmail.com>
2016-08-03 17:57:07 -0700

    add mlab uri

--- index.js
+++ index.js
@@ -3,12 +3,32 @@ var School = require('./models/school');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser')
 var multer  = require('multer')
-
+var mongodb = require("mongodb");
 var app = express();
 var router = express.Router(); 
 
-var uri = 'mongodb://127.0.0.1:27017/test';
-mongoose.connect(uri);
+
+// Create a database variable outside of the database connection callback to reuse the connection pool in your app.
+var db;
+
+// Connect to the database before starting the application server.
+mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+
+  // Save database object from the callback for reuse.
+  db = database;
+  console.log("Database connection ready");
+
+  // Initialize the app.
+  var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+  });
+});
+
 
 
 // middleware to use for all requests
@@ -170,10 +190,5 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json({limit: '5mb'}));
 app.use('/api',router);
 app.use(express.static(__dirname + '/publish'));
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
-
 module.exports = router;
 
